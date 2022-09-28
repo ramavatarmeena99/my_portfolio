41cd789135c6743d99ed99a2e35e352f4cc52f69
ramavatarmeena99 <meenaramavatar48@gmail.com>
2022-09-28 11:51:03 +0530

    somediscription fixed

--- src/data.js
+++ src/data.js
@@ -8,7 +8,7 @@ export const data = [
     id: 1,
     projectName: "Contact List App",
     aboutForProject:
-      "Evolution Aquarium is based on Craig Reynold's Steering Behaviors and Flocking System, and It implements a Genetic Algorithm and mutations. Daniel Shiffman showcased this on his Youtube Channel at TheCodingTrain. And I was pleased about the humble feedback; sir Daniel Shiffman gave to me. It just boosted my confidence more and more, and motivated me to make this project better.",
+      "Hello everyone, I have made this fully Responsive Simple Contact App In React Redux... I Have used ReactJS as a frontend. I Am not using any backend in this Project, so a just client-side application  This is a CRUD application where users can be able to add, update and delete the contact...Here, I uses most important React JS features - React Hooks",
     link: "https://addcontactlist.netlify.app/",
     projectImg: ContactList,
     projectGithubLink: "https://github.com/ramavatarmeena99/Contact_List_App",
@@ -17,7 +17,7 @@ export const data = [
     id: 2,
     projectName: "Score Board In ReactJs",
     aboutForProject:
-      "Evolution Aquarium is based on Craig Reynold's Steering Behaviors and Flocking System, and It implements a Genetic Algorithm and mutations. Daniel Shiffman showcased this on his Youtube Channel at TheCodingTrain. And I was pleased about the humble feedback; sir Daniel Shiffman gave to me. It just boosted my confidence more and more, and motivated me to make this project better.",
+      "Hello everyone , I have made this Fully Responsive Cricket Match Score Keeper in React JS , keeping in mind the rules of the game...   Tried to write clean code and implemented DRY rule, wherever needed  Here, I uses most important React JS features   -Custom Hooks    Match Score Keeper includes some below features: 1-You can choose the Teams name and number of Overs yourself... 2-Match results can be seen only after both the teams have completed all the overs or 10 wickets...  3-If all the overs or 10 wickets of any one team fall, then that team cannot play further... 4-The team with the most runs will be the winner",
     link: "https://score-keeping-app.netlify.app/",
     projectImg: ScoreKeeping,
     projectGithubLink:
@@ -27,7 +27,7 @@ export const data = [
     id: 3,
     projectName: "Youtube Clone",
     aboutForProject:
-      "Evolution Aquarium is based on Craig Reynold's Steering Behaviors and Flocking System, and It implements a Genetic Algorithm and mutations. Daniel Shiffman showcased this on his Youtube Channel at TheCodingTrain. And I was pleased about the humble feedback; sir Daniel Shiffman gave to me. It just boosted my confidence more and more, and motivated me to make this project better.",
+      "Hello everyone presenting Youtube Clone Responsive Web Aplication...  => Youtube Clone app uses Youtube Data API & Frontend based on React JS...  => Here, I uses most important React JS features-Custom Hooks... => -React Routers Youtube Clone includes some below features: 1-Browse videos by search... 2-Sort by most popular... 3-Get recommended videos related to search video...  4-Comments related to search video ",
     link: "https://theyoutube.netlify.app/",
     projectImg: YoutubeClone,
     projectGithubLink: "https://github.com/ramavatarmeena99/Youtube-Clone",
@@ -36,7 +36,7 @@ export const data = [
     id: 4,
     projectName: "Dribble Clone",
     aboutForProject:
-      "Evolution Aquarium is based on Craig Reynold's Steering Behaviors and Flocking System, and It implements a Genetic Algorithm and mutations. Daniel Shiffman showcased this on his Youtube Channel at TheCodingTrain. And I was pleased about the humble feedback; sir Daniel Shiffman gave to me. It just boosted my confidence more and more, and motivated me to make this project better.",
+      "Dribble clone using HTML5, CSS3 and Vanilla JavaScript.      ",
     link: "https://ramavatarmeena99.github.io/DribbleSiteClone/",
     projectImg: DribbleClone,
     projectGithubLink: "https://github.com/ramavatarmeena99/DribbleSiteClone",
